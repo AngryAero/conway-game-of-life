5cabb95b95b56c1946691854d7d05d78d727ebe0
AngryAero <133472167+AngryAero@users.noreply.github.com>
2025-05-25 11:50:52 +0800

    Updated App Icon

--- src/AppFrame.java
+++ src/AppFrame.java
@@ -1,10 +1,13 @@
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 
 public class AppFrame extends JFrame {
     public AppFrame() {
+        ImageIcon image = new ImageIcon(getClass().getResource("/resources/app-icon.png"));
+        setIconImage(image.getImage());
         setTitle("Conway's Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
